e5d35eeef2269196c7de1c64c3edb301479025a6
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-11-04 22:32:33 -0800

    chapter 8 finished

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import java.util.UUID;
+import java.util.Date;
 
 /**
  * Created by tonyk_000 on 10/29/2015.
@@ -10,9 +11,29 @@ public class Crime {
     private UUID mId;
     private String mTitle;
 
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    private Date mDate;
+    private boolean mSolved;
+
     public Crime() {
         //generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
