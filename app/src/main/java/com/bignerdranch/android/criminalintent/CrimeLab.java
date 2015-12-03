3de447f6595d1ddeb63e00377ff2789e8a59c036
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-12-03 15:51:05 -0800

    crime item deletes but does not pop back to previous activity

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -30,6 +30,10 @@ public class CrimeLab {
         mCrimes.add(c);
     }
 
+    public void removeCrime(Crime c){
+        mCrimes.remove(c);
+    }
+
     public List<Crime> getCrimes(){
         return mCrimes;
     }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -1,11 +1,24 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+
+import java.util.UUID;
 
 /**
  * Created by tonyk_000 on 11/9/2015.
  */
 public class CrimeListActivity extends SingleFragmentActivity {
+
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+
+
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
