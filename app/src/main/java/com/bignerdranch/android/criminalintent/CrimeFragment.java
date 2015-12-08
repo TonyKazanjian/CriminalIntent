1baa5cacc57032f4eea5d3d8803952f73e209faa
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-12-08 13:12:40 -0800

    challenge 14 completed

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -9,6 +9,9 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -51,6 +54,8 @@ public class CrimeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+
+        setHasOptionsMenu(true);
     }
 
     //updates CrimeLab's copy of the crime when it gets modified in CrimeFragment
@@ -107,6 +112,25 @@ public class CrimeFragment extends Fragment {
         });
 
         return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch(menuItem.getItemId()){
+            case R.id.menu_item_delete_crime:
+                CrimeLab.get(null).removeCrime(mCrime);
+                getActivity().finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
+
     }
 
     //retrieving date extra from DatePickerFragment, setting the date on Crime, and refreshing the text on the date button
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -42,6 +42,12 @@ public class CrimeLab {
         mDatabase.insert(CrimeTable.NAME, null, values);
     }
 
+    public void removeCrime(Crime crime){
+        String uuidString = crime.getId().toString();
+        mDatabase.delete(CrimeTable.NAME,CrimeTable.Cols.UUID + " = ?",
+                new String[]{uuidString});;
+    }
+
     //Adding code to query for all crimes, walk the cursor, and populate a Crime list
     public List<Crime> getCrimes(){
         List<Crime> crimes = new ArrayList<>();
